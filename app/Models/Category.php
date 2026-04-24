a71472443e60f41ad88b1930add5393b5fb05ea4
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-04-24 19:26:08 +0300

    feat: implment create & update & delete category model
    implment create & update & delete category model using modal

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -14,4 +17,10 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    // Relationships
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'category_id', 'id');
+    }
 }
